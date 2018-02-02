a125eaf082aa05b9be46dffe56b11ecf2c3d2895
parthosengupta <partho.s.sengupta@gmail.com>
2018-02-02 17:22:47 +0530

    Tools
    Added tool menu items for creating modified fungus dialogs.

---
+++ Assets/Game/Common/Scripts/Editor/FungusTools.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace com.nidb.utils
+{
+	public class FungusTools 
+	{
+		[MenuItem("NIDB/Fungus Tools/Add Say With Response Dialog")]
+		public static void SpawnSayWithResponseDialog()
+		{
+			GameObject go = SpawnPrefabInScene("Prefabs/PfSayDialogWithResponseTemplate");
+			go.transform.position = Vector3.zero;
+		}
+
+		[MenuItem("NIDB/Fungus Tools/Add Menu with Say Dialog")]
+		public static void SpawnMenuWithSayDialog()
+		{
+			GameObject go = SpawnPrefabInScene("Prefabs/PfMenuWithSayTemplate");
+			go.transform.position = Vector3.zero;
+		}
+		
+		public static GameObject SpawnPrefabInScene(string prefabName)
+		{
+			GameObject prefab = Resources.Load<GameObject>(prefabName);
+			if (prefab == null)
+			{
+				return null;
+			}
+			
+			GameObject go = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+			PrefabUtility.DisconnectPrefabInstance(go);
+			
+			SceneView view = SceneView.lastActiveSceneView;
+			if (view != null)
+			{
+				Camera sceneCam = view.camera;
+				Vector3 pos = sceneCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 10f));
+				pos.z = 0f;
+				go.transform.position = pos;
+			}
+			
+			Selection.activeGameObject = go;
+			
+			Undo.RegisterCreatedObjectUndo(go, "Create Object");
+			
+			return go;
+		}
+	}
+}
