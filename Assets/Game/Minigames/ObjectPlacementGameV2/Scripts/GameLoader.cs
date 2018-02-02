dd9b7593286dc1e9c7c1814c3b36ac3ed1df48d0
parthosengupta <partho.s.sengupta@gmail.com>
2018-02-02 16:19:41 +0530

    Object Placement Template
    Separated game prefab so it can be customized.
    Changed flowchart to load game prefab instead of scene.

---
+++ Assets/Game/Minigames/ObjectPlacementGameV2/Scripts/GameLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.nidb.games.placementgame
+{
+	public class GameLoader : MonoBehaviour 
+	{
+		[SerializeField]
+		// Use this for initialization
+		void Start () 
+		{
+		
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			
+		}
+	}
+}
