df550c91a740337b6cec2793715529daf7b3556c
vaibhavi26 <joshi.vaibhavi2612@gmail.com>
2018-02-22 18:25:09 +0530

    Integration/timer

---
+++ Assets/Game/Minigames/HiddenObjectGameTemplate/Scripts/CountdownTimerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimerScript : MonoBehaviour {
+
+	public float timeLeft = 60;
+	float min;
+	float sec;
+
+	public Text cdTimer;
+
+	void Start()
+	{
+		
+	}
+
+
+	void Update()
+	{
+			
+		timeLeft -= Time.deltaTime;
+		min =Mathf.Floor(timeLeft / 60);
+		sec =Mathf.Floor(timeLeft % 60);
+		if (sec >= 10) {
+			cdTimer.text = "0" + min.ToString () + ":" + sec.ToString ();
+		} else {
+			cdTimer.text = "0" + min.ToString () + ":" +"0"+ sec.ToString ();
+		}
+			
+		if(timeLeft <1)
+		{
+            Invoke("SceneLoader", 0.9f);
+		}
+	}
+
+	public void setTimeLeft(int n)
+	{
+		timeLeft = timeLeft - n;
+	}
+
+    void SceneLoader()
+    {
+        SceneManager.LoadScene("HiddenObjectGameScene");
+    }
+}
---
+++ Assets/Game/Minigames/HiddenObjectGameTemplate/Scripts/TimerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour {
+
+    private int mWrongClickCount;
+    
+	// Use this for initialization
+	void Start () {
+        mWrongClickCount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (mWrongClickCount == 3)
+        {
+            this.GetComponent<CountdownTimerScript>().setTimeLeft(5);
+            mWrongClickCount = 0;
+        
+        }
+	}
+
+    public void setWrongClickCount(int clickIndex)
+    {
+        mWrongClickCount += clickIndex;
+    }
+}
