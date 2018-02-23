b25795d1b91f49cfdb3dc90a6d76aefec2733033
vaibhavi26 <joshi.vaibhavi2612@gmail.com>
2018-02-23 18:17:13 +0530

    Popoup

--- Assets/Game/Minigames/HiddenObjectGameTemplate/Scripts/InvalidObjectScript.cs
+++ Assets/Game/Minigames/HiddenObjectGameTemplate/Scripts/InvalidObjectScript.cs
@@ -1,40 +1,47 @@
-﻿using System.Collections;
+﻿using com.nidb.games.hiddenobjectgame;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InvalidObjectScript : MonoBehaviour {
+namespace com.nidb.games.hiddenobjectgame
+{
+    public class InvalidObjectScript : MonoBehaviour
+    {
 
-    [SerializeField]
-    private GameObject mGameCanvasObject;
-    // Use this for initialization
-    void Start () {
-        mGameCanvasObject = GameObject.FindGameObjectWithTag("HoGGameCanvas");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        /*if (Input.GetMouseButtonDown(0))
+        [SerializeField]
+        private GameObject mGameCanvasObject;
+        // Use this for initialization
+        void Start()
         {
-            //Get the mouse position on the screen and send a raycast into the game world from that position.
-            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+            mGameCanvasObject = GameObject.FindGameObjectWithTag("HoGGameCanvas");
+        }
 
-            //If something was hit, the RaycastHit2D.collider will not be null.
-            if (hit.collider.gameObject == this.gameObject)
+        // Update is called once per frame
+        void Update()
+        {
+            /*if (Input.GetMouseButtonDown(0))
             {
-                Debug.Log("Invalid Click");
-            }
-            else if(hit.collider.gameObject.tag=="valid")
+                //Get the mouse position on the screen and send a raycast into the game world from that position.
+                Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+                //If something was hit, the RaycastHit2D.collider will not be null.
+                if (hit.collider.gameObject == this.gameObject)
                 {
-                Debug.Log("Valid Click");
-            }*/
+                    Debug.Log("Invalid Click");
+                }
+                else if(hit.collider.gameObject.tag=="valid")
+                    {
+                    Debug.Log("Valid Click");
+                }*/
 
 
         }
-    private void OnMouseDown()
-    {
-        mGameCanvasObject.GetComponent<TimerController>().setWrongClickCount(1);
-    }
+        private void OnMouseDown()
+        {
+            mGameCanvasObject.GetComponent<TimerController>().setWrongClickCount(1);
+        }
 
+    }
 }
 
--- Assets/Game/Minigames/HiddenObjectGameTemplate/Scripts/TimerController.cs
+++ Assets/Game/Minigames/HiddenObjectGameTemplate/Scripts/TimerController.cs
@@ -2,27 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimerController : MonoBehaviour {
-
-    private int mWrongClickCount;
-    
-	// Use this for initialization
-	void Start () {
-        mWrongClickCount = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (mWrongClickCount == 3)
-        {
-            this.GetComponent<CountdownTimerScript>().setTimeLeft(5);
-            mWrongClickCount = 0;
-        
-        }
-	}
-
-    public void setWrongClickCount(int clickIndex)
+namespace com.nidb.games.hiddenobjectgame
+{
+    public class TimerController : MonoBehaviour
     {
-        mWrongClickCount += clickIndex;
+
+        private int mWrongClickCount;
+        [SerializeField]
+        private GameObject mPenaltyPopUpPanel;
+
+        // Use this for initialization
+        void Start()
+        {
+            mWrongClickCount = 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (mWrongClickCount == 3)
+            {
+
+                this.GetComponent<CountdownTimerScript>().setTimeLeft(5);
+                Time.timeScale = 0.5f;
+                mPenaltyPopUpPanel.SetActive(true);
+                mWrongClickCount = 0;
+                Invoke("PanelDeactivator", 0.75f);
+
+
+            }
+        }
+
+        public void setWrongClickCount(int clickIndex)
+        {
+            mWrongClickCount += clickIndex;
+        }
+
+        void PanelDeactivator()
+        {
+            mPenaltyPopUpPanel.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
