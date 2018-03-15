64765f32769c7eb70cc565487309b211a88228c5
parthosengupta <partho.s.sengupta@gmail.com>
2018-03-15 14:50:34 +0530

    Hidden Object
    Added Help and Home buttons.

---
+++ Assets/Game/Minigames/HiddenObjectGameTemplate/Scripts/HiddenObjectHUDHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace com.nidb.games.hiddenobjectgame
+{
+	/**
+	 * Handles HUD events that are not directly related to the game
+	 */
+	public class HiddenObjectHUDHandler : MonoBehaviour 
+	{
+		[Header("Home Button")]
+		[SerializeField]
+		private Button _btnHome;
+		[SerializeField]
+		private string _homeSceneName;
+		[Header("Help Button")]
+		[SerializeField]
+		private Button _btnHelp;
+		[SerializeField]
+		private Transform _pnlHelp;
+
+		// Use this for initialization
+		void Start () 
+		{
+			if(_btnHome != null)
+				_btnHome.onClick.AddListener(() => SceneManager.LoadScene(_homeSceneName));
+			if(_btnHelp != null)
+				_btnHelp.onClick.AddListener(() => _pnlHelp.gameObject.SetActive(true));
+		}
+	}
+}
